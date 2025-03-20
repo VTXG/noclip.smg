fd9fb17900eedd1097e231507534a0cdab6f2294
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2025-03-19 20:18:54 -0500

    add ctors

--- src/CANM.ts
+++ src/CANM.ts
@@ -4,12 +4,46 @@ import * as Studio from "./Studio";
 export class Track {
     values: rust.Frame[];
     usesinglescope: boolean;
+    constructor(usesinglescope: boolean = true) {
+        this.values = []
+        this.usesinglescope = usesinglescope
+    }
+}
+
+export class CANMHeader {
+    frame_count: number;
+    frame_type: string;
+    magic: string;
+    offset: number;
+    unk1: number;
+    unk2: number;
+    unk3: number;
+    unk4: number;
+    constructor(frame_type : rust.FrameType = rust.FrameType.CANM) {
+        this.magic = 'ANDO'
+        if (frame_type === rust.FrameType.CANM) {
+            this.frame_type = 'CANM'
+        } else if (frame_type === rust.FrameType.CKAN) {
+            this.frame_type = 'CKAN'
+        }
+        this.unk1 = 0;
+        this.unk2 = 0
+        this.unk3 = 0;
+        this.unk4 = 0;
+        this.frame_count = 0;
+        this.offset = 0;
+    }
 }
 
 export class CANM {
-    header: rust.CANMHeader;
+    header: CANMHeader;
     tracks: Map<string, Track[]>;
     isfullframes: boolean;
+    constructor(frame_type = rust.FrameType.CANM, isfullframes: boolean = false) {
+        this.header = new CANMHeader(frame_type);
+        this.tracks = new Map<string, Track[]>();
+        this.isfullframes = isfullframes
+    }
 }
 
 export function canm_to_js(data: Uint8Array): CANM {
